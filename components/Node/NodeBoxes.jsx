f4a3dce4fb38857b9456719d8b25b5e8e7bc62f6
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-07-23 09:01:57 +0530

    Sync ai dev deployment 0.1.3

--- components/Node/NodeBoxes.jsx
+++ components/Node/NodeBoxes.jsx
@@ -5,7 +5,7 @@ const NodeBoxes = ({ layer }) => {
     <div className="flex p-10">
       <div className="text-white bg-[#151515] p-4 rounded-3xl">
         <div className="border-b py-3 border-slate-500">
-          {layer.title && <h1 className="text-center 2xl:text-xl">{layer.title}</h1>}
+          {layer.title && <h1 className="text-center 2xl:text-2xl">{layer.title}</h1>}
         </div>
         <div>{layer.items.header && <p>{layer.items.header}</p>}</div>
         {layer.items.map((item, index) => (
@@ -17,7 +17,7 @@ const NodeBoxes = ({ layer }) => {
             )}
             <div className=" bg-gradient-to-b p-[1px] from-blue-500 to-slate-500 rounded-xl my-3">
               <div className="flex items-center justify-start p-3 gap-1 rounded-xl bg-[#151515]">
-                <div className="text-md 2xl:text-xl gap-2 text-blue-500">{item.icon}</div>
+                <div className="text-md 2xl:text-2xl gap-2 text-blue-500">{item.icon}</div>
                 <p>{item.text}</p>
               </div>
             </div>
--- components/Node/NodeBud.jsx
+++ components/Node/NodeBud.jsx
@@ -4,18 +4,18 @@ import CoinImage from "@/assets/syncnode/coin.png"; // Update this path to the a
 
 const NodeBud = () => {
   return (
-    <div className="relative flex items-center justify-center min-h-screen">
+    <div className="relative flex items-center justify-center h-dvh z-10">
       <div className="relative">
         <div className="absolute inset-0 rounded-full bg-[#121212] flex items-center justify-center">
-          <div className="absolute w-[80px] h-[80px] md:w-[120px] md:h-[120px] lg:w-[160px] lg:h-[160px] rounded-full bg-[#121212] flex items-center justify-center z-10">
+          <div className="absolute w-[80px] h-[80px] md:w-[120px] md:h-[120px] 2xl:w-[160px] 2xl:h-[160px] rounded-full bg-[#121212] flex items-center justify-center z-10">
             <Image src={CoinImage} alt="Center Icon" width={100} height={100} md={150} lg={200} />
           </div>
         </div>
         <div className="absolute w-full h-full rounded-full border-4 border-gray-800 animate-pulse"></div>
       </div>
-      <div className="absolute w-[135px] h-[135px] md:w-[200px] md:h-[200px] lg:w-[270px] lg:h-[270px] border-[40px] md:border-[60px] lg:border-[80px] border-gray-800 rounded-full"></div>
-      <div className="absolute w-[165px] h-[165px] md:w-[250px] md:h-[250px] lg:w-[330px] lg:h-[330px] border-[2px] md:border-[3px] lg:border-[4px] border-gray-800 rounded-full"></div>
-      <div className="absolute w-[180px] h-[180px] md:w-[270px] md:h-[270px] lg:w-[360px] lg:h-[360px] border-[2px] md:border-[3px] lg:border-[4px] border-gray-800 rounded-full"></div>
+      <div className="absolute w-[135px] h-[135px] md:w-[200px] md:h-[200px] 2xl:w-[270px] 2xl:h-[270px] border-[40px] md:border-[60px] 2xl:border-[80px] border-gray-800 rounded-full"></div>
+      <div className="absolute w-[165px] h-[165px] md:w-[250px] md:h-[250px] 2xl:w-[330px] 2xl:h-[330px] border-[2px] md:border-[3px] 2xl:border-[4px] border-gray-800 rounded-full"></div>
+      <div className="absolute w-[180px] h-[180px] md:w-[270px] md:h-[270px] 2xl:w-[360px] 2xl:h-[360px] border-[2px] md:border-[3px] 2xl:border-[4px] border-gray-800 rounded-full"></div>
     </div>
   );
 };
